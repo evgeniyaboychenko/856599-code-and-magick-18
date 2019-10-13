2e3f296a82cf9e4b77e9761676517b1ec35d120a
evgeniyaboychenko <evgeniyaboychenko@gmail.com>
2019-10-13 23:46:02 +0700

    работа сетью

---
+++ js/backend.js
@@ -0,0 +1,89 @@
+'use strict';
+(function () {
+  var load = function (onLoad, onError) {
+    var url = 'https://js.dump.academy/code-and-magick/data';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      var error;
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+        case 400:
+          error = 'Неверный запрос';
+          break;
+        case 401:
+          error = 'Пользователь не авторизован';
+          break;
+        case 404:
+          error = 'Ничего не найдено';
+          break;
+        default:
+          error = 'Статус ответа' + xhr.status + ' ' + xhr.statusText;
+      }
+
+      if (error) {
+        onError(error);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+    xhr.open('GET', url);
+    xhr.send();
+  };
+
+  var save = function (data, onLoad, onError) {
+    var url = 'https://js.dump.academy/code-and-magick';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      var error;
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+        case 400:
+          error = 'Неверный запрос';
+          break;
+        case 401:
+          error = 'Пользователь не авторизован';
+          break;
+        case 404:
+          error = 'Ничего не найдено';
+          break;
+        default:
+          error = 'Статус ответа' + xhr.status + ' ' + xhr.statusText;
+      }
+
+      if (error) {
+        onError(error);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+    xhr.open('POST', url);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+})();
--- js/setup.js
+++ js/setup.js
@@ -1,24 +1,24 @@
 'use strict';
 (function () {
   var WIZARD_ITEM_TEMPLATE = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-  var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  // var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  // var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   var NUMBER_WIZARDS = 4;
 
-  var createCharacters = function () {
-    var characters = [];
-    for (var i = 0; i < NUMBER_WIZARDS; i++) {
-      characters[i] = {
-        name: window.getArrayData(NAMES) + ' ' + window.getArrayData(SURNAMES),
-        coatColor: window.getArrayData(COAT_COLORS),
-        eyesColor: window.getArrayData(EYES_COLORS)
-      };
-    }
-    return characters;
-  };
+  // var createCharacters = function () {
+  //   var characters = [];
+  //   for (var i = 0; i < NUMBER_WIZARDS; i++) {
+  //     characters[i] = {
+  //       name: window.getArrayData(NAMES) + ' ' + window.getArrayData(SURNAMES),
+  //       coatColor: window.getArrayData(COAT_COLORS),
+  //       eyesColor: window.getArrayData(EYES_COLORS)
+  //     };
+  //   }
+  //   return characters;
+  // };
 
   var createWizard = function (wizard) {
     var wizardItem = WIZARD_ITEM_TEMPLATE.cloneNode(true);
@@ -26,22 +26,43 @@
     var coatColorWizard = wizardItem.querySelector('.wizard-coat');
     var eyesColorWizard = wizardItem.querySelector('.wizard-eyes');
     nameWizard.textContent = wizard.name;
-    coatColorWizard.style.fill = wizard.coatColor;
-    eyesColorWizard.style.fill = wizard.eyesColor;
+    coatColorWizard.style.fill = wizard.colorCoat;
+    eyesColorWizard.style.fill = wizard.colorEyes;
     return wizardItem;
   };
 
-  var createWizards = function () {
+  var createWizards = function (data) {
     var fragment = document.createDocumentFragment();
-    var wizardsData = createCharacters();
+    var wizards = randomArray(NUMBER_WIZARDS, data);
     for (var i = 0; i < NUMBER_WIZARDS; i++) {
-      fragment.appendChild(createWizard(wizardsData[i]));
+      fragment.appendChild(createWizard(wizards[i]));
     }
     return fragment;
   };
 
+  var randomArray = function (n, array) {
+    var smallArray = [];
+    for (var i = 0; i < n; i++) {
+      smallArray[i] = (array.splice(Math.floor(Math.random() * array.length), 1))[0];
+    }
+    return smallArray;
+  };
+
   var wizardList = document.querySelector('.setup-similar-list');
-  wizardList.appendChild(createWizards());
+
+  var onError = function (message) {
+    var div = document.createElement('div');
+    var header = document.querySelector('header');
+    div.style = 'z-index: 100;  text-align: center; background-color: red; position: absolute; fontSize: 30px; width: 800px; left: 50%; margin-left: -400px; top: 40px; font-size: 30px';
+    div.textContent = message;
+    header.appendChild(div);
+  };
+
+  var onRenderWizards = function (data) {
+    wizardList.appendChild(createWizards(data));
+  };
+
+  window.backend.load(onRenderWizards, onError);
 
   var setupSimilarWizard = document.querySelector('.setup-similar');
   setupSimilarWizard.classList.remove('hidden');
@@ -59,4 +80,15 @@
   window.colorize(coatColorMyWizard, fieldInputCoatColor, COAT_COLORS);
   window.colorize(eyesColorMyWizard, fieldInputEyesColor, EYES_COLORS);
   window.colorize(fireballColorMyWizard, fieldInputFireballColor, FIREBALL_COLOR);
+
+
+  var onSaveSuccess = function () {
+    document.querySelector('.setup').classList.add('hidden');
+  };
+
+  var form = document.querySelector('.setup').querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    window.backend.save(new FormData(form), onSaveSuccess, onError);
+  });
 })();
