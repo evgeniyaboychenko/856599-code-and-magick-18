21cc5d09aaa57268f7a618874d0a4fb9ab092212
evgeniyaboychenko <evgeniyaboychenko@gmail.com>
2019-10-14 10:55:03 +0700

    исправление замечаний

--- js/backend.js
+++ js/backend.js
@@ -1,31 +1,34 @@
 'use strict';
 (function () {
-  var load = function (onLoad, onError) {
-    var url = 'https://js.dump.academy/code-and-magick/data';
+  var onStatusConnectionServer = function (xhr, onLoad, onError) {
+    var error;
+    switch (xhr.status) {
+      case 200:
+        onLoad(xhr.response);
+        break;
+      case 400:
+        error = 'Неверный запрос';
+        break;
+      case 401:
+        error = 'Пользователь не авторизован';
+        break;
+      case 404:
+        error = 'Ничего не найдено';
+        break;
+      default:
+        error = 'Статус ответа' + xhr.status + ' ' + xhr.statusText;
+    }
+
+    if (error) {
+      onError(error);
+    }
+  };
+
+  var getXHR = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
-      var error;
-      switch (xhr.status) {
-        case 200:
-          onLoad(xhr.response);
-          break;
-        case 400:
-          error = 'Неверный запрос';
-          break;
-        case 401:
-          error = 'Пользователь не авторизован';
-          break;
-        case 404:
-          error = 'Ничего не найдено';
-          break;
-        default:
-          error = 'Статус ответа' + xhr.status + ' ' + xhr.statusText;
-      }
-
-      if (error) {
-        onError(error);
-      }
+      onStatusConnectionServer(xhr, onLoad, onError);
     });
 
     xhr.addEventListener('error', function () {
@@ -37,47 +40,19 @@
     });
 
     xhr.timeout = 10000; // 10s
+    return xhr;
+  };
+
+  var load = function (onLoad, onError) {
+    var url = 'https://js.dump.academy/code-and-magick/data';
+    var xhr = getXHR(onLoad, onError);
     xhr.open('GET', url);
     xhr.send();
   };
 
   var save = function (data, onLoad, onError) {
     var url = 'https://js.dump.academy/code-and-magick';
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      var error;
-      switch (xhr.status) {
-        case 200:
-          onLoad(xhr.response);
-          break;
-        case 400:
-          error = 'Неверный запрос';
-          break;
-        case 401:
-          error = 'Пользователь не авторизован';
-          break;
-        case 404:
-          error = 'Ничего не найдено';
-          break;
-        default:
-          error = 'Статус ответа' + xhr.status + ' ' + xhr.statusText;
-      }
-
-      if (error) {
-        onError(error);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000; // 10s
+    var xhr = getXHR(onLoad, onError);
     xhr.open('POST', url);
     xhr.send(data);
   };
